9daa36967a6764dad1908db24ccb08e7ade324cc
Luis Sandoval Monestel <luisasandovalcoder@gmail.com>
2023-06-29 22:25:26 -0600

    CopyFromUri

---
+++ Azure Storage/StorageCopy/StorageCopy/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs.Specialized;
+
+
+Console.WriteLine("Moving file from container 1 to container 2");
+
+var connectionString = "";
+
+var sourceContainer = "test";
+var targetContainer = "second";
+
+var sourceFile = "background_jarvis.jpg";
+var targetFile = "background_jarvis-COPY2.jpg";
+
+var sourceClient = new BlockBlobClient(connectionString, sourceContainer, sourceFile);
+var targetClient = new BlockBlobClient(connectionString,targetContainer, targetFile);
+
+await targetClient.StartCopyFromUriAsync(sourceClient.Uri);
+
+
+Console.WriteLine("File moved");
+Console.ReadLine();
+
