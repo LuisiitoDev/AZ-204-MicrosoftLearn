131953c547f0b08ffa553b570a97c9e330bd10f0
LuisiitoDev <luissandoval1897@hotmail.com>
2023-07-01 17:23:30 -0600

    Azure Active Directory

---
+++ Azure Active Directory/MicrosoftGraphListOfUsers/MicrosoftGraphListOfUsers/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+
+var scopes = new[] { "https://graph.microsoft.com/.default" };
+string tenantId = "";
+string clientSecret = "";
+string clientId = "";
+
+var options = new TokenCredentialOptions()
+{
+    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+};
+
+var clientSecretCredential = new ClientSecretCredential(
+    tenantId, clientId, clientSecret, options);
+
+var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+var users = await graphClient.Users.GetAsync();
+
+foreach (var user in users!.Value!)
+{
+    Console.WriteLine(user.DisplayName + " , " + user.GivenName);
+}
+
