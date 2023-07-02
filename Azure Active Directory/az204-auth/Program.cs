c586721561fff1bfdbf51099be36ea88e8cecc3e
LuisiitoDev <luissandoval1897@hotmail.com>
2023-07-01 21:32:55 -0600

    Azure Key Vault

---
+++ Azure Active Directory/az204-auth/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Identity.Client;
+
+const string clientId = "bf25271b-fd76-452e-9f69-15812d6ee8b2";
+const string tenantId = "4fd96409-78dc-4e07-9b56-8095f2a1f2a5";
+
+var app = PublicClientApplicationBuilder
+.Create(clientId)
+.WithAuthority(AzureCloudInstance.AzurePublic,tenantId)
+.WithRedirectUri("http://localhost")
+.Build();
+
+string[] scopes = { "user.read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token: {result.AccessToken}");
+
---
+++ AzureKeyVault/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+string keyVaultUrl = "";
+
+var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+
+string secretName = "mysecret";
+
+KeyVaultSecret secret = await client.GetSecretAsync(secretName);
+
+Console.WriteLine("The value of the secret is: " + secret.Value);
+
+
