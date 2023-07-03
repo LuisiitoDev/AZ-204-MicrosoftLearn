27deebc8019632d939b39b3e8925eaa486245798
LuisiitoDev <luissandoval1897@hotmail.com>
2023-07-02 20:01:08 -0600

    Azure Cache Redis

---
+++ Caching y CDN/RedisDotNet/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+
+string CacheConnection = "";
+
+var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+{
+    return ConnectionMultiplexer.Connect(CacheConnection);
+});
+
+
+IDatabase cache = lazyConnection.Value.GetDatabase();
+
+Console.WriteLine($"Reading Cache: {cache.StringGet("Session33")}");
+Console.WriteLine($"Writing Cache: {cache.StringSet("Session33", "Hello world from cache!")}");
+Console.WriteLine($"Reading Cache: {cache.StringGet("Session33")}");
+cache.KeyExpire("Session33", DateTime.Now.AddMinutes(1));
+
+
+lazyConnection.Value.Dispose();
