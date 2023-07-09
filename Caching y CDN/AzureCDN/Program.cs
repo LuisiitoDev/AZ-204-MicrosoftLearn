4bd8a386497266e229ddb8b1129ca9d720241fda
LuisiitoDev <luissandoval1897@hotmail.com>
2023-07-09 08:56:37 -0600

    OpenID Authentication

---
+++ Caching y CDN/AzureCDN/Program.cs
@@ -0,0 +1,4 @@
+﻿using Microsoft.Azure.Management.Cdn.Fluent;
+using Microsoft.Rest;
+
+CdnManagementClient cdn = new CdnManagementClient(new TokenCredentials("access-token")){ SubscriptionId = "suscription-id" };
