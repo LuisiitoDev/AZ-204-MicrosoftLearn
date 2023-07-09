4e8161d12a34cafb52fcba031d1e9abeae8e1a2d
LuisiitoDev <luissandoval1897@hotmail.com>
2023-07-09 12:17:07 -0600

    Azure Service Bus

---
+++ Labs/AzureServiceBus/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Messaging.ServiceBus;
+
+
+string serviceBusConnectionString = "Endpoint=sb://sbnamespaceluisiitodev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eMtVwnn1UjEbFtD1lOWVjldc+6XYfb898+ASbD8BJKo=";
+string queueName = "messagequeue";
+int numOfMessages = 3;
+
+ServiceBusClient client = default!;
+ServiceBusSender sender = default!;
+
+client = new(serviceBusConnectionString);
+sender = client.CreateSender(queueName);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i <= numOfMessages; i++)
+{
+    if(!batch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+        throw new Exception($"The message {i} is too large to fit in the batch.");
+}
+
+await sender.SendMessagesAsync(batch);
+Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+
+await sender.DisposeAsync();
+await client.DisposeAsync();
---
+++ Labs/AzureServiceBusReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connection = "Endpoint=sb://sbnamespaceluisiitodev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eMtVwnn1UjEbFtD1lOWVjldc+6XYfb898+ASbD8BJKo=";
+string queueName = "messagequeue";
+
+ServiceBusClient client = default!;
+ServiceBusProcessor processor = default!;
+
+client = new(connection);
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+processor.ProcessMessageAsync += async (ProcessMessageEventArgs args) =>
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+    await args.CompleteMessageAsync(args.Message);
+};
+
+processor.ProcessErrorAsync += (ProcessErrorEventArgs args) =>
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+};
+
+await processor.StartProcessingAsync();
+Console.WriteLine("Wait for a minute and then press any key to end the processing");
+Console.ReadKey();
+
+Console.WriteLine("\nStopping the receiver...");
+await processor.StopProcessingAsync();
+Console.WriteLine("Stopped receiving messages");
+
+
+await processor.DisposeAsync();
+await client.DisposeAsync();
